0b55e4aa98bd81485cba05418aa0aeb74cdfeacf
Satvik Saha <sahasatvik@gmail.com>
2019-10-17 22:35:24 +0530

    Added speed and accuracy counters

--- typingtest.js
+++ typingtest.js
@@ -1,45 +1,69 @@
 var i = 0;
 var start = -1;
+var typed = '';
+var typedcorrect = '';
 
 function init() {
-    document.getElementById('testtext-untyped').innerHTML = text;
-    document.getElementById("testtext-untyped").scrollIntoView();
+    i = 0;
+    start = -1;
+    typed = '';
+    updateTestText();
+    document.getElementById('wpm').innerHTML = '--';
+    document.getElementById('accuracy').innerHTML = '--';
 }
 
-function update() {
-    if (start > 0) {
+function updateTestText() {
+    var t = '';
+    typedcorrect = '';
+    for (var j = 0; j < i; j++) {
+        c = text.charAt(j);
+        if (c == typed.charAt(j)) {
+            t += c;
+            typedcorrect += c;
+        } else {
+            if (c == ' ') {c = '_';}
+            t += '<span class="testtext-wrong">' + c + '</span>';
+        }
+    }
+    document.getElementById('testtext-typed').innerHTML = t;
+    document.getElementById('testtext-untyped').innerHTML = text.substring(i);
+    document.getElementById('testtext-untyped').scrollIntoView();
+}
+
+function updateConsole() {
+    if (start > 0 && i > 0) {
         var current = (new Date()).getTime();
         var ms = current - start;
-        var wpm = Math.round(1000 * 60 * characters(i) / (5 * ms));
+        var wpm = Math.round(1000 * 60 * characters(typedcorrect) / (5 * ms));
+        var accuracy = Math.round(typedcorrect.length * 100.0 / typed.length);
         document.getElementById('wpm').innerHTML = wpm + 'wpm';
+        document.getElementById('accuracy').innerHTML = accuracy + '%';
     }
 }
 
 function keyDownEventListener(e) {
-    if (start < 0) {
-        start = (new Date()).getTime();
-    }
     if (e.keyCode == 8) {
         i--;
-    } else if (e.key == text[i]) {
+        typed = typed.substring(0, i);
+    } else if (e.keyCode == 27) {
+        init();
+    } else if (e.key.length == 1) {
         i++;
+        typed += e.key;
+        if (start < 0) {
+            start = (new Date()).getTime();
+        }
     }
     
     if (i < 0) { i = 0;}
     if (i > text.length) {i = 0;}
 
-    t = cut(i);
-    document.getElementById('testtext-untyped').innerHTML = t[1];
-    document.getElementById('testtext-typed').innerHTML = t[0];
-    document.getElementById("testtext-untyped").scrollIntoView();
+    updateTestText();
+    updateConsole()
 }
 
-function cut(i) {
-    return [text.substring(0, i), text.substring(i)];
+function characters(s) {
+    return s.replace(/[^a-zA-Z0-9]/g,'').length;
 }
 
-function characters(i) {
-    return text.substring(0, i).replace(/[^a-zA-Z0-9]/g,'').length;
-}
-
-var text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus pulvinar elementum integer enim neque volutpat. Etiam erat velit scelerisque in. Nibh mauris cursus mattis molestie a iaculis at erat pellentesque. Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc sed. Non arcu risus quis varius quam quisque id diam vel. Amet luctus venenatis lectus magna. Nunc scelerisque viverra mauris in aliquam sem fringilla. Cursus risus at ultrices mi tempus imperdiet nulla malesuada. Feugiat nibh sed pulvinar proin gravida hendrerit. Sollicitudin nibh sit amet commodo nulla. Sagittis id consectetur purus ut faucibus pulvinar elementum integer enim. Eu consequat ac felis donec et odio pellentesque.\nNunc aliquet bibendum enim facilisis gravida neque convallis a. A scelerisque purus semper eget duis at. Facilisis leo vel fringilla est ullamcorper eget nulla facilisi. Nunc congue nisi vitae suscipit tellus mauris a diam maecenas. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Nunc aliquet bibendum enim facilisis gravida neque convallis. Neque convallis a cras semper auctor neque. Mattis vulputate enim nulla aliquet porttitor lacus luctus. Platea dictumst quisque sagittis purus. Risus ultricies tristique nulla aliquet enim tortor at auctor urna. Venenatis cras sed felis eget velit aliquet sagittis id consectetur. Duis tristique sollicitudin nibh sit amet.\nAc auctor augue mauris augue neque gravida. Malesuada proin libero nunc consequat interdum varius sit amet mattis. Eu mi bibendum neque egestas. Rhoncus aenean vel elit scelerisque mauris pellentesque. Ornare massa eget egestas purus viverra accumsan in nisl nisi. Odio tempor orci dapibus ultrices. Fringilla ut morbi tincidunt augue. Fames ac turpis egestas integer. Aliquet eget sit amet tellus cras adipiscing. Consectetur a erat nam at lectus urna duis convallis. Purus semper eget duis at tellus at. Lectus arcu bibendum at varius vel pharetra vel turpis. Lectus urna duis convallis convallis tellus. Est velit egestas dui id ornare arcu. Neque convallis a cras semper auctor neque vitae tempus quam. Dictumst quisque sagittis purus sit amet volutpat.\nOrci porta non pulvinar neque laoreet suspendisse interdum consectetur libero. Eget est lorem ipsum dolor. Felis bibendum ut tristique et egestas quis. Nec nam aliquam sem et. Rhoncus dolor purus non enim. Sollicitudin tempor id eu nisl nunc mi. Felis imperdiet proin fermentum leo vel orci porta non. Donec ac odio tempor orci. Vulputate eu scelerisque felis imperdiet proin fermentum. Eu feugiat pretium nibh ipsum consequat nisl vel. Sit amet luctus venenatis lectus magna. Pellentesque nec nam aliquam sem et tortor. Orci eu lobortis elementum nibh tellus molestie nunc non blandit. Vitae congue eu consequat ac. Nisl suscipit adipiscing bibendum est ultricies. Vitae justo eget magna fermentum iaculis eu non diam phasellus. Elementum integer enim neque volutpat. In fermentum posuere urna nec tincidunt praesent. Duis tristique sollicitudin nibh sit amet. Luctus accumsan tortor posuere ac ut.\nQuam viverra orci sagittis eu volutpat. Non sodales neque sodales ut etiam sit amet. Sit amet venenatis urna cursus eget nunc scelerisque viverra. Ultricies mi quis hendrerit dolor. Cras pulvinar mattis nunc sed blandit libero volutpat sed cras. Sed augue lacus viverra vitae congue eu consequat. Lacus luctus accumsan tortor posuere. A lacus vestibulum sed arcu. Vitae auctor eu augue ut lectus arcu bibendum. Quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Auctor neque vitae tempus quam. Orci eu lobortis elementum nibh tellus molestie nunc non blandit. Aliquam sem fringilla ut morbi tincidunt augue interdum. Aliquam id diam maecenas ultricies. Elementum curabitur vitae nunc sed velit dignissim.';
+let text = `The thousand injuries of Fortunato I had borne as I best could; but when he ventured upon insult, I vowed revenge. You, who so well know the nature of my soul, will not suppose, however, that I gave utterance to a threat. At length I would be avenged; this was a point definitely settled - but the very definitiveness with which it was resolved precluded the idea of risk. I must not only punish, but punish with impunity. A wrong is unredressed when retribution overtakes its redresser. It is equally unredressed when the avenger fails to make himself felt as such to him who has done the wrong. It must be understood that neither by word nor deed had I given Fortunato cause to doubt my good-will. I continued, as was my wont, to smile in his face, and he did not perceive that my smile now was at the thought of his immolation. He had a weak point - this Fortunato - although in other regards he was a man to be respected and even feared. He prided himself on his connoisseurship in wine. Few Italians have the true virtuoso spirit. For the most part their enthusiasm is adopted to suit the time and opportunity - to practise imposture upon the British and Austrian millionaires. In painting and gemmary, Fortunato, like his countrymen, was a quack - but in the matter of old wines he was sincere. In this respect I did not differ from him materially: I was skillful in the Italian vintages myself, and bought largely whenever I could.`;
